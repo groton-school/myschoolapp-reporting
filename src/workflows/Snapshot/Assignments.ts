b13efbf903480ef0e17cd3b357712e369871b480
Seth Battis <seth@battis.net>
2024-11-21 15:25:46 -0500

    fewer tabs
    Close #2. Seems like, even though the tab should have been closed, it may have still been waiting for network connection and not reaching the close command. Or something.
    
    Fixed ejs build with copyfiles (a bandaid) so that I could skip using `tsx` and just test with `node`.

--- src/workflows/Snapshot/Assignments.ts
+++ src/workflows/Snapshot/Assignments.ts
@@ -45,55 +45,58 @@ export async function capture(
     )
   ).json();
 
-  const host = new URL(page.url()).hostname;
   const assignments: api.Assignment2.Response[] = [];
-  let complete = false;
-  function completion(timeout = 30000): Promise<void> {
-    let duration = 0;
-    return new Promise((resolve, reject) => {
-      function pollCompletion() {
-        if (complete) {
-          resolve();
-        } else {
-          if (duration < timeout) {
-            duration += 100;
-            setTimeout(pollCompletion, 100);
+  if (assignmentList.count > 0) {
+    const host = new URL(page.url()).hostname;
+    let complete = false;
+    function completion(timeout = 30000): Promise<void> {
+      let duration = 0;
+      return new Promise((resolve, reject) => {
+        function pollCompletion() {
+          if (complete) {
+            resolve();
           } else {
-            reject('timeout expired');
+            if (duration < timeout) {
+              duration += 100;
+              setTimeout(pollCompletion, 100);
+            } else {
+              reject('timeout expired');
+            }
           }
         }
-      }
-      pollCompletion();
-    });
-  }
-
-  const assPage = await page.browser().newPage();
-
-  // https://stackoverflow.com/a/55422938/294171
-  await assPage.setRequestInterception(true);
-  assPage.on('request', (request) => {
-    request.continue();
-  });
-  assPage.on('requestfinished', async (request) => {
-    if (
-      request.redirectChain().length === 0 &&
-      /\/api\/assignment2\/UserAssignmentDetailsGetAllData/.test(request.url())
-    ) {
-      const response = request.response();
-      assignments.push(await response?.json());
-      complete = true;
+        pollCompletion();
+      });
     }
-  });
 
-  for (const assignment of assignmentList.value) {
-    complete = false;
-    await assPage.goto(
-      `https://${host}/lms-assignment/assignment/assignment-preview/${assignment.index_id}`
-    );
-    await completion();
+    const assPage = await page.browser().newPage();
+
+    // https://stackoverflow.com/a/55422938/294171
+    await assPage.setRequestInterception(true);
+    assPage.on('request', (request) => {
+      request.continue();
+    });
+    assPage.on('requestfinished', async (request) => {
+      if (
+        request.redirectChain().length === 0 &&
+        /\/api\/assignment2\/UserAssignmentDetailsGetAllData/.test(
+          request.url()
+        )
+      ) {
+        const response = request.response();
+        assignments.push(await response?.json());
+        complete = true;
+      }
+    });
+
+    for (const assignment of assignmentList.value) {
+      complete = false;
+      await assPage.goto(
+        `https://${host}/lms-assignment/assignment/assignment-preview/${assignment.index_id}`
+      );
+      await completion();
+    }
+    await assPage.close();
   }
-  await assPage.close();
-
   spinner.succeed('Assignments captured');
   return assignments;
 }
