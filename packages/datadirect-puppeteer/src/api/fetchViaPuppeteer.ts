d5bbcc04687123bd0d302eae798d0dae05504e01
Seth Battis <sbattis@ml-tc-24-sbattis.local>
2024-12-26 23:21:05 -0500

    feat(datadirect-puppeteer): Enable richer debugging of bad responses

--- packages/datadirect-puppeteer/src/api/fetchViaPuppeteer.ts
+++ packages/datadirect-puppeteer/src/api/fetchViaPuppeteer.ts
@@ -1,6 +1,16 @@
 import { Endpoint } from 'datadirect/dist/api.js';
 import { Page } from 'puppeteer';
 
+type FetchError = {
+  error: unknown;
+  response: Response;
+  body: string;
+} & Endpoint.FetchParams;
+
+function isFetchError(e: unknown): e is FetchError {
+  return e != null && typeof e === 'object' && 'error' in e && 'body' in e;
+}
+
 export function fetchViaPuppeteer<
   P extends Endpoint.Payload,
   R extends Endpoint.Response
@@ -8,15 +18,46 @@ export function fetchViaPuppeteer<
   return async (
     page: Page,
     payload: P,
-    pathParams: Record<string, string | number | boolean> = {}
+    pathParams: Record<string, string | number | boolean> = {},
+    logBodyOnError = false
   ) => {
     const { input, init } = module.prepare(payload, page.url());
-    return await page.evaluate(
+    const result: R | FetchError = await page.evaluate(
       async (params) => {
         const { input, init } = params;
-        return (await (await fetch(input, init)).json()) as R;
+        let body: R | undefined | string = undefined;
+        let response: Response | undefined = undefined;
+        try {
+          response = await fetch(input, init);
+          body = await response.text();
+          return JSON.parse(body) as R;
+        } catch (error) {
+          return {
+            error,
+            input,
+            init,
+            response: {
+              url: response?.url,
+              redirected: response?.redirected,
+              type: response?.type,
+              status: response?.status,
+              statusText: response?.statusText,
+              headers: response?.headers
+            },
+            body
+          } as FetchError;
+        }
       },
       { input: Endpoint.preparePath(input, pathParams), init }
     );
+    if (isFetchError(result)) {
+      const { error, body, ...rest } = result;
+      if (logBodyOnError) {
+        console.error(rest);
+        console.error(body);
+      }
+      throw error;
+    }
+    return result;
   };
 }
