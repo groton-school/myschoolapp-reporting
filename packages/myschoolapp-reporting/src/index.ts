405f7227d612d9925d356ceb898a5cc50a85ffaf
Seth Battis <seth@battis.net>
2025-01-14 20:15:47 -0500

    chore: remove deprecated myschoolapp-reporting package

--- packages/myschoolapp-reporting/src/index.ts
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-
-console.log(chalk.cyan.bold('myschoolapp-reporting has been deprecated'));
-console.log(`Please use ${chalk.yellow.bold('msar')} instead.`);
-console.log('https://npmjs.com/package/msar');
