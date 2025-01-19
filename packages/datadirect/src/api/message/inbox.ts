e5aed051f06f5d68da483888324cf968d7fb3816
Seth Battis <seth@battis.net>
2025-01-19 07:50:21 -0500

    feat(datadirect): Add optional pageSize prop to endpoint

--- packages/datadirect/src/api/message/inbox.ts
+++ packages/datadirect/src/api/message/inbox.ts
@@ -4,5 +4,7 @@ import { Payload } from './inbox/Payload.js';
 export * from './inbox/Payload.js';
 export * from './inbox/Response.js';
 
+export const pageSize = 20;
+
 export const prepare: Endpoint.Prepare<Payload> = (payload, base?: string) =>
-  Endpoint.prepare({ payload, base, path: '/api/message/inbox', pageSize: 20 });
+  Endpoint.prepare({ payload, base, path: '/api/message/inbox', pageSize });
