83d3b4576f6fb0306ca8792c30b5a9d33e91d57b
Seth Battis <seth@battis.net>
2025-03-01 15:56:36 -0500

    refactor(msar): remove workflows (now qui-cli plugins)

--- packages/msar/src/workflows/Download/Args/flags.ts
+++
@@ -1,5 +0,0 @@
-import * as common from '../../../common.js';
-
-export const defaults = {};
-
-export const flags = common.Args.flags;
--- packages/msar/src/workflows/Download/Downloader/Strategy.ts
+++
@@ -1,8 +0,0 @@
-import { DownloadData, DownloadError } from '../Cache.js';
-
-export interface Strategy {
-  download(
-    url: string,
-    filename?: string
-  ): Promise<DownloadData | DownloadError>;
-}
