adbbf9ae189f73ce857a0daebd061be2b4589947
Seth Battis <seth@battis.net>
2025-03-09 17:10:25 -0400

    feat(datadirect): ContentItem.Text.Photos

--- packages/datadirect/src/api/datadirect/common/ContentItem/Text.ts
+++ packages/datadirect/src/api/datadirect/common/ContentItem/Text.ts
@@ -1,4 +1,61 @@
-import { DateTimeString, HTMLString } from '@battis/descriptive-types';
+import {
+  DateTimeString,
+  HTMLString,
+  PathString,
+  URLString
+} from '@battis/descriptive-types';
+
+type ImageOp = {
+  FileId: number;
+  MetaKey: number;
+  /** e.g. `"440px"` */
+  MetaValue: string;
+  FileTypeId: number;
+  FileLocationId: number;
+};
+
+type Photo = {
+  Id: number;
+  FilePath: PathString;
+  ThumbFilePath: PathString;
+  ZoomFilePath: PathString;
+  OriginalFilePath: PathString;
+  LargeFilenameUrl: PathString;
+  LargeFilename: string;
+  LargeHeight: number;
+  LargeWidth: number;
+  ThumbFilenameUrl: PathString;
+  ThumbFilename: string;
+  ThumbWidth: number;
+  ThumbHeight: number;
+  ZoomFilenameUrl: string;
+  ZoomWidth: number;
+  ZoomHeight: number;
+  OriginalFilenameUrl: PathString;
+  OriginalFilename: string;
+  OriginalWidth: number;
+  OriginalHeight: number;
+  EditedWidth: number;
+  EditedHeight: number;
+  PhotoEditSettings: string;
+  Title: string;
+  Caption: string;
+  photo_alttext: string;
+  hover_alttext: string;
+  LongDescription: HTMLString;
+  TagList: [];
+  FileEdited: boolean;
+  IsHoverPhoto: boolean;
+  PhotoEapEnabled: boolean;
+  ImageOps: ImageOp[];
+  OriginalFilenameEditedUrl: PathString;
+  LargeFilenameEditedUrl: PathString;
+  ZoomFilenameEditedUrl: PathString;
+  ThumbFilenameEditedUrl: PathString;
+  PhotoTypeId: number;
+  PhotoWidth: number;
+  PhotoPK: number;
+};
 
 export type Text = {
   Description: string;
@@ -16,6 +73,8 @@ export type Text = {
   IsBackgroundJobTask: boolean;
   IsPhotoProcessed: boolean;
   PhotoCorrupted: boolean;
+  FileName?: PathString;
+  Photos?: Photo[];
 };
 
 export type Content = Text[];
