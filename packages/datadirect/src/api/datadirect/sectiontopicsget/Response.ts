04ba6f75239c6b83667fb23d3e16519502b603c8
Seth Battis <seth@battis.net>
2025-03-10 17:47:36 -0400

    fix(datadirect): update api.datadirect.sectiontopicsget.Item to reflect actual response

--- packages/datadirect/src/api/datadirect/sectiontopicsget/Response.ts
+++ packages/datadirect/src/api/datadirect/sectiontopicsget/Response.ts
@@ -1,35 +1,16 @@
-import {
-  DateString,
-  NumericBoolean,
-  NumericTimestamp
-} from '@battis/descriptive-types';
+import { DateString, NumericTimestamp } from '@battis/descriptive-types';
 
 export type Item = {
   AllowCopy: boolean;
   AllowEdit: boolean;
-  ContextLabelId: number;
-  CreatedByUser: string;
   Description?: string | null;
-  DiscussionThread: boolean;
-  EditorOfContent: string;
   ExpireDate?: DateString | null;
-  GroupName: string;
-  LayoutId: number;
-  LockedInd: number;
   Name: string;
-  Primary: boolean;
   PublishDate: DateString;
   PublishDateTicks: NumericTimestamp;
-  SchoolYear: string;
-  SectionId: number;
-  ShareWarningInd: NumericBoolean;
   ThumbFilename?: string | null;
-  TopicGroup: string;
   TopicID: number;
   TopicIndexID: number;
-  ViewerIsContentEditor: NumericBoolean;
-  ViewerIsManagerInd: NumericBoolean;
-  ViewerIsOwnerInd: NumericBoolean;
 };
 
 export type Response = Item[];
