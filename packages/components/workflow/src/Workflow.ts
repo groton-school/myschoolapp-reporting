bbd6fc8a50f6e648557b5f9678c4fbe4a7486658
Seth Battis <seth@battis.net>
2025-03-10 21:24:22 -0400

    feat(workflow)!: remove concurrentThreads option
    BREAKING: concurrentThreads is now called concurrency and is managed by @msar/rate-limiter

--- packages/components/workflow/src/Workflow.ts
+++ packages/components/workflow/src/Workflow.ts
@@ -4,7 +4,6 @@ import * as Plugin from '@battis/qui-cli.plugin';
 export type Configuration = Plugin.Configuration & {
   ignoreErrors?: boolean;
   logRequests?: boolean;
-  concurrentThreads?: number;
 };
 
 export const name = '@msar/workflow';
@@ -12,8 +11,7 @@ export const src = import.meta.dirname;
 
 const props = {
   ignoreErrors: true,
-  logRequests: false,
-  concurrentThreads: 10
+  logRequests: false
 };
 
 export function ignoreErrors() {
@@ -24,17 +22,9 @@ export function logRequests() {
   return props.logRequests;
 }
 
-export function concurrentThreads() {
-  return props.concurrentThreads;
-}
-
 export function configure(config: Configuration = {}) {
   props.ignoreErrors = Plugin.hydrate(config.ignoreErrors, props.ignoreErrors);
   props.logRequests = Plugin.hydrate(config.logRequests, props.logRequests);
-  props.concurrentThreads = Plugin.hydrate(
-    config.concurrentThreads,
-    props.concurrentThreads
-  );
 }
 
 export function options(): Plugin.Options {
@@ -47,13 +37,6 @@ export function options(): Plugin.Options {
       logRequests: {
         description: `Log fetch requests and responses for analysis and debugging (default: ${Colors.value(props.logRequests)})`,
         default: props.logRequests
-      }
-    },
-    num: {
-      concurrentThreads: {
-        description: `Maximum number of concurrent threads (default: ${Colors.value(props.concurrentThreads)})`,
-        default: props.concurrentThreads,
-        validate: (value?) => value && value > 0
       }
     }
   };
