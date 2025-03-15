81c697af77774b22a447e66dfb9e8877f4bc762c
Seth Battis <seth@battis.net>
2025-03-15 15:34:39 -0400

    refactor(types.snapshot): Assignment.Item

--- packages/types/snapshot/src/Assignments.ts
+++ packages/types/snapshot/src/Assignments.ts
@@ -1,5 +1,7 @@
 import { School } from '@oauth2-cli/sky-api';
 import { api } from 'datadirect';
 
-export type Data = (api.Assignment2.UserAssignmentDetailsGetAllData.Response &
-  Partial<School.Assignment>)[];
+export type Item = api.Assignment2.UserAssignmentDetailsGetAllData.Response &
+  Partial<School.Assignment>;
+
+export type Data = Item[];
