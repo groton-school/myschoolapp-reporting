04e651f3b3da9545aa51762bd706546f7605bb30
Seth Battis <seth@battis.net>
2025-03-15 22:11:38 -0400

    fix(types.archive): more flexible Annotation type

--- packages/types/archive/src/Annotation.ts
+++ packages/types/archive/src/Annotation.ts
@@ -3,7 +3,10 @@ import { DateTimeString, PathString } from '@battis/descriptive-types';
 export type Annotation = {
   original: PathString;
   accessed: DateTimeString;
-} & ({ localPath: PathString; filename: string } | { error: string });
+} & (
+  | { localPath: PathString; filename: string; error: never }
+  | { localPath: never; filename: never; error: string }
+);
 
 export function isAnnotated(obj: object): obj is Annotation {
   return obj !== null && 'original' in obj && 'accessed' in obj;
