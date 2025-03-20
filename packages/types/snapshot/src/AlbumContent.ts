0949b26b04a6619cdfecbefce69afc99f40993a5
Seth Battis <seth@battis.net>
2025-03-20 14:13:34 -0400

    fix(types.snapshot): update AlbumContent to reflect actual stored data

--- packages/types/snapshot/src/AlbumContent.ts
+++ packages/types/snapshot/src/AlbumContent.ts
@@ -1,21 +1,24 @@
 import { PathString } from '@battis/descriptive-types';
 import { api } from 'datadirect';
 
-export type Item<T = PathString> = Omit<
-  api.media.AlbumFilesGet.Response,
-  | 'FilenameUrl'
-  | 'ThumbFilenameUrl'
-  | 'OriginalFilenameUrl'
-  | 'FilenameEditedUrl'
-  | 'ThumbFilenameEditedUrl'
-  | 'OriginalFilenameEditedUrl'
-> & {
-  FilenameUrl: T;
-  ThumbFilenameUrl: T;
-  OriginalFilenameUrl: T;
-  FilenameEditedUrl: T;
-  ThumbFilenameEditedUrl: T;
-  OriginalFilenameEditedUrl: T;
+export type Item<T = PathString> = {
+  AlbumId: number;
+  Content: (Omit<
+    api.media.AlbumFilesGet.Response,
+    | 'FilenameUrl'
+    | 'ThumbFilenameUrl'
+    | 'OriginalFilenameUrl'
+    | 'FilenameEditedUrl'
+    | 'ThumbFilenameEditedUrl'
+    | 'OriginalFilenameEditedUrl'
+  > & {
+    FilenameUrl: T;
+    ThumbFilenameUrl: T;
+    OriginalFilenameUrl: T;
+    FilenameEditedUrl: T;
+    ThumbFilenameEditedUrl: T;
+    OriginalFilenameEditedUrl: T;
+  })[];
 };
 
 export type Data<T = PathString> = Item<T>[];
