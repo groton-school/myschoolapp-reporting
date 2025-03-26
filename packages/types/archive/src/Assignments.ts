906ee9961aa901a1c69705d37b0cddbfccda8d2c
Seth Battis <seth@battis.net>
2025-03-26 08:33:33 -0400

    fix(types.archive): correct typing for Assignment.Data

--- packages/types/archive/src/Assignments.ts
+++ packages/types/archive/src/Assignments.ts
@@ -2,4 +2,4 @@ import * as Snapshot from '@msar/types.snapshot';
 import { PotentialAnnotation } from './Annotation.js';
 
 export type Item<T = PotentialAnnotation> = Snapshot.Assignments.Item<T>;
-export type Data<T = PotentialAnnotation> = Snapshot.Assignments.Data<T>;
+export type Data<T = PotentialAnnotation> = Item<T>[];
