1f8da63ab59957d369dc2fa9e108a42f858f8ac3
Seth Battis <seth@battis.net>
2025-04-13 17:36:38 -0400

    refactor(msar): push command hierarchy into @battis/qui-cli.structured

--- packages/msar/src/index.ts
+++ packages/msar/src/index.ts
@@ -1,25 +1,8 @@
-import { Colors } from '@battis/qui-cli.colors';
-import { Log } from '@battis/qui-cli.log';
-import fs from 'node:fs';
+import { build } from '@battis/qui-cli.structured';
 import path from 'node:path';
 
-const [command] = process.argv.splice(2, 1);
-
-const commands = fs
-  .readdirSync(path.join(import.meta.dirname, 'commands'))
-  .filter((f) => !/\.map$/.test(f))
-  .map((command) => command.replace(/\.[jt]s$/, ''));
-const ext =
-  path.basename(path.resolve(import.meta.dirname)) === 'dist' ? 'js' : 'ts';
-const filename = `${command}.${ext}`;
-if (commands.includes(command)) {
-  process.argv[1] = path.join(import.meta.dirname, 'commands', filename);
-  import(process.argv[1]);
-} else if (/^-?-h(elp)?$/.test(command)) {
-  Log.info(`Usage:
-  ${path.basename(process.argv[1])} command -h --help
-
-  Available commands: ${commands.map((command) => Colors.value(command)).join(', ')}
-
-  -h --help         Usage`);
-}
+build({
+  fileName: import.meta.dirname,
+  commandDirPath: path.join(import.meta.dirname, 'commands'),
+  commandName: 'msar'
+});
