20c2fd1b27ea531e18a106cc56ccf0c241070704
Seth Battis <seth@battis.net>
2025-06-27 15:28:22 -0400

    fix: updated bin to be npm-friendly

--- packages/msar/src/index.ts
+++ packages/msar/src/index.ts
@@ -3,6 +3,5 @@ import path from 'node:path';
 
 build({
   fileName: import.meta.dirname,
-  commandDirPath: path.join(import.meta.dirname, 'commands'),
-  commandName: 'msar'
+  commandDirPath: path.join(import.meta.dirname, 'commands')
 });
