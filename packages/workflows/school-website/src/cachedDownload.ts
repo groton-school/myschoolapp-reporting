35bed81258443aeda4153686cc2b842de25eab4e
Seth Battis <seth@battis.net>
2026-01-19 21:07:50 -0500

    refactor: fix file-checking logic

--- packages/workflows/school-website/src/cachedDownload.ts
+++ packages/workflows/school-website/src/cachedDownload.ts
@@ -17,46 +17,50 @@ export async function cachedDownload(url?: URLString, minSizeBytes = 0) {
       Output.outputPath(),
       localPath
     );
-    let acceptCached = fs.existsSync(filePath);
-    const stat = fs.statSync(filePath);
-    if (stat.size < minSizeBytes) {
-      Log.debug(
-        `${Colors.path(localPath)} is smaller than ${minSizeBytes} bytes (${stat.size} bytes)`
-      );
-      acceptCached = false;
-    }
-    if (acceptCached) {
-      Log.debug(
-        `Cached ${Colors.url(url)} already present at ${Colors.path(path.join(Output.outputPath(), localPath), Colors.keyword)}`
-      );
-      spinner.info(Colors.path(localPath, Colors.keyword));
-      return localPath;
-    } else {
-      try {
-        const filePath = await Output.writeFetchedFile({
-          url,
-          stream: (await fetch(url)).body as ReadableStream,
-          outputPath: Output.outputPath()
-        });
+    if (fs.existsSync(filePath)) {
+      const stat = fs.statSync(filePath);
+      if (stat.size < minSizeBytes) {
         Log.debug(
-          `Downloaded ${Colors.url(url)} to ${Colors.path(filePath, Colors.keyword)}`
+          `${Colors.path(localPath)} is smaller than ${minSizeBytes} bytes (${stat.size} bytes)`
         );
-        const stat = fs.statSync(
-          Output.filePathFromOutputPath(Output.outputPath(), filePath)
+      } else {
+        Log.debug(
+          `Cached ${Colors.url(url)} already present at ${Colors.path(path.join(Output.outputPath(), localPath), Colors.keyword)}`
         );
+        spinner.info(Colors.path(localPath, Colors.keyword));
+        return localPath;
+      }
+    }
+    try {
+      const localPath = await Output.writeFetchedFile({
+        url,
+        stream: (await fetch(url)).body as ReadableStream,
+        outputPath: Output.outputPath()
+      });
+      const filePath = Output.filePathFromOutputPath(
+        Output.outputPath(),
+        localPath
+      );
+      if (fs.existsSync(filePath)) {
+        Log.debug(
+          `Downloaded ${Colors.url(url)} to ${Colors.path(localPath, Colors.keyword)}`
+        );
+        const stat = fs.statSync(filePath);
         if (stat.size < minSizeBytes) {
           throw new Error(
             `${Colors.path(localPath)} is smaller than ${minSizeBytes} bytes (${stat.size} bytes)`
           );
         }
-        spinner.succeed(Colors.path(filePath, Colors.value));
-        return filePath;
-      } catch (error) {
-        const message = Colors.error(`${error}: ${Colors.url(url)}`);
-        Log.debug(message);
-        spinner.fail(message);
-        return undefined;
+      } else {
+        throw new Error(`${Colors.path(localPath)} does not exist`);
       }
+      spinner.succeed(Colors.path(localPath, Colors.value));
+      return localPath;
+    } catch (error) {
+      const message = Colors.error(`${error}: ${Colors.url(url)}`);
+      Log.debug(message);
+      spinner.fail(message);
+      return undefined;
     }
   }
 }
