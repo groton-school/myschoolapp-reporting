02b6fc6357229c94881cca3d83ff9764e63f36b9
Seth Battis <seth@battis.net>
2026-01-19 21:06:13 -0500

    fix: use more reliable stream-handling approach

--- packages/components/output/src/Output/writeFetchedFile.ts
+++ packages/components/output/src/Output/writeFetchedFile.ts
@@ -4,8 +4,6 @@ import { Log } from '@qui-cli/log';
 import { Root } from '@qui-cli/root';
 import fs from 'node:fs';
 import path from 'node:path';
-import { Readable } from 'node:stream';
-import { finished } from 'node:stream/promises';
 import { ReadableStream } from 'node:stream/web';
 
 type Options = {
@@ -27,11 +25,7 @@ export async function writeFetchedFile({ url, stream, outputPath }: Options) {
     if (stream instanceof Buffer) {
       fs.writeFileSync(streamPath, stream);
     } else {
-      await finished(
-        Readable.fromWeb(stream as ReadableStream).pipe(
-          fs.createWriteStream(streamPath)
-        )
-      );
+      await fs.promises.writeFile(streamPath, stream);
     }
     Log.debug(`Saved ${Colors.url(url)} to ${Colors.path(localPath)}`);
     return localPath;
